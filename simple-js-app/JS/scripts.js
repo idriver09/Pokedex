b7f79a7c6ea82b4966eb3675d1f98327ac556787
idriver09 <126793168+idriver09@users.noreply.github.com>
2023-03-11 09:16:57 -0500

    changed height decimals to have a 0 in front of the decimal

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -1,22 +1,22 @@
 let pokemonList = [
-   
-    {
-    Name: 'Weedle',
-    Type: 'bug/poison',
-    Height: .3,
-    Abilities: ['Sheid-dust','Run-away']},
-  
-    {
-    Name: 'Zubat',
-    Type:'poison/flying',
-    Height: .8,
-    Abilities: ['Inner-focus','Infiltrator']},
-   
-    {
-    Name: 'Spearow',
-    Type: 'flying/normal',
-    Height: .3,
-    Abilities: ['Keen-eye','Sniper']}
+  {
+    Name: "Weedle",
+    Type: "bug/poison",
+    Height: 0.3,
+    Abilities: ["Sheid-dust", "Run-away"],
+  },
 
+  {
+    Name: "Zubat",
+    Type: "poison/flying",
+    Height: 0.8,
+    Abilities: ["Inner-focus", "Infiltrator"],
+  },
 
+  {
+    Name: "Spearow",
+    Type: "flying/normal",
+    Height: 0.3,
+    Abilities: ["Keen-eye", "Sniper"],
+  },
 ];
