36cbb5a1051927365d0b9b18873503fc584a66d6
idriver09 <126793168+idriver09@users.noreply.github.com>
2023-03-14 09:35:56 -0400

    added a loop that displays pokemon names and height / and looks for the size of the pokemon and renders wether it is large, medium, or small

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -9,7 +9,7 @@ let pokemonList = [
   {
     Name: "Zubat",
     Type: "poison/flying",
-    Height: 0.8,
+    Height: 1.8,
     Abilities: ["Inner-focus", "Infiltrator"],
   },
 
@@ -20,3 +20,45 @@ let pokemonList = [
     Abilities: ["Keen-eye", "Sniper"],
   },
 ];
+
+/*The following loop lists the pokemon names and height values. 
+It also checks the height of the pokemon and renders whether it is large, medium, or small. */
+
+let space = " ";
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].Height < 1) {
+    document.write(
+      pokemonList[i].Name +
+        "-" +
+        space +
+        "Height" +
+        space +
+        pokemonList[i].Height +
+        space +
+        "(Small Pokemon)" +
+        space
+    );
+  } else if (pokemonList[i].Height > 0 && pokemonList[i].Height < 0.5) {
+    document.write(
+      pokemonList[i].Name +
+        "-" +
+        "Height" +
+        space +
+        pokemonList[i].Height +
+        space +
+        "(Medium Pokemon)" +
+        space
+    );
+  } else {
+    document.write(
+      pokemonList[i].Name +
+        "-" +
+        "Height" +
+        space +
+        pokemonList[i].Height +
+        space +
+        "(Wow that is Big!)" +
+        space
+    );
+  }
+}
