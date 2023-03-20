10f911cda7daac1e962984d977e4b14690a0196d
idriver09 <126793168+idriver09@users.noreply.github.com>
2023-03-20 08:18:08 -0400

    replaced for loop with forEach on pokemonlist

--- simple-js-app/JS/scripts.js
+++ simple-js-app/JS/scripts.js
@@ -21,44 +21,55 @@ let pokemonList = [
   },
 ];
 
+let space = "  ";
+pokemonList.forEach(function(pokemonDetails){
+  document.write('<h5>' + 'Name: ' + '</h5>' + pokemonDetails.Name + space +'<h5>' + 'Type: '+ '</h5>' + pokemonDetails.Type + space +'<h5>' + 'Height: '+ '</h5>' + pokemonDetails.Height + space + '<h5>' +'Abilities: '+ '</h5>'
+   + space + pokemonDetails.Abilities + '<br>');
+})
 /*The following loop lists the pokemon names and height values. 
 It also checks the height of the pokemon and renders whether it is large, medium, or small. */
 
-let space = " ";
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].Height < 1) {
-    document.write(
-      pokemonList[i].Name +
-        "-" +
-        space +
-        "Height" +
-        space +
-        pokemonList[i].Height +
-        space +
-        "(Small Pokemon)" +
-        space
-    );
-  } else if (pokemonList[i].Height > 0 && pokemonList[i].Height < 0.5) {
-    document.write(
-      pokemonList[i].Name +
-        "-" +
-        "Height" +
-        space +
-        pokemonList[i].Height +
-        space +
-        "(Medium Pokemon)" +
-        space
-    );
-  } else {
-    document.write(
-      pokemonList[i].Name +
-        "-" +
-        "Height" +
-        space +
-        pokemonList[i].Height +
-        space +
-        "(Wow that is Big!)" +
-        space
-    );
-  }
-}
+// let space = " ";
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].Height < 1) {
+//     document.write(
+//       "<h4>" +
+//       pokemonList[i].Name +
+//         "-" +
+//         space +
+//         "Height" +
+//         space +
+//         pokemonList[i].Height +
+//         space +
+//         "(Small Pokemon)" +
+//         space
+//         + "</h4>"
+//     );
+//   } else if (pokemonList[i].Height > 0 && pokemonList[i].Height < 0.5) {
+//     document.write(
+//       "<h4>" +
+//       pokemonList[i].Name +
+//         "-" +
+//         "Height" +
+//         space +
+//         pokemonList[i].Height +
+//         space +
+//         "(Medium Pokemon)" +
+//         space
+//         + "</h4>"
+//     );
+//   } else {
+//     document.write(
+//       "<h4>" +
+//       pokemonList[i].Name +
+//         "-" +
+//         "Height" +
+//         space +
+//         pokemonList[i].Height +
+//         space +
+//         "(Wow that is Big!)" +
+//         space
+//         + "</h4>"
+//     );
+//   }
+// }
